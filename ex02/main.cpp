a9eb7ef99a77f54d1d0b4470894483b544917e15
jorvarea <jorvarea@c3r4s6.42malaga.com>
2025-07-22 21:25:31 +0200

    fix ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,14 +4,14 @@
 int main() {
     std::string str = "HI THIS IS BRAIN";
     std::string *stringPTR = &str;
-    std::string **stringREF = &stringPTR;
+    std::string &stringREF = str;
 
     std::cout << "Memory address of the string variable: " << &str << std::endl;
-    std::cout << "Memory address held by stringPTR: " << &stringPTR << std::endl;
+    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
     std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
 
     std::cout << "Value of the string variable: " << str << std::endl;
-    std::cout << "Value pointed to by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
     std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
 
     return 0;
