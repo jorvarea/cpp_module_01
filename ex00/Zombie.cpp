4a16ab5cb2f0cb8f2acf26cfc736ea07087a2669
jorvarea <jorvarea@c3r4s6.42malaga.com>
2025-07-22 19:44:25 +0200

    fix unnecessary constructor

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -1,6 +1,5 @@
 #include "Zombie.hpp"
 
-Zombie::Zombie() : _name("") {}
 Zombie::Zombie(std::string name) : _name(name) {}
 Zombie::~Zombie() {
     std::cout << "Zombie " << _name << " has been destroyed." << std::endl;
