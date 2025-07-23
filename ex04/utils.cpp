1522ea79457a7bd17edbb473156a754b9a5e16f0
jorvarea <jorvarea@users.noreply.github.com>
2025-07-23 16:29:21 +0200

    add extra check to ex04 (i uploaded the files last commit)

--- ex04/utils.cpp
+++ ex04/utils.cpp
@@ -11,6 +11,8 @@ void fill_buffer(std::ifstream& file, std::string& buffer) {
 }
 
 void find_and_replace(std::string& content, std::string s1, std::string s2) {
+    if (s1.empty()) return;
+
     size_t pos = 0;
     while ((pos = content.find(s1, pos)) != std::string::npos) {
         content.erase(pos, s1.length());
